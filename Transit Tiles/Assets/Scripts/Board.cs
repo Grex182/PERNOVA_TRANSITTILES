d0430bb2499366610b7a24a2672ff5799983dafd
Grex182 <grecoinigo.yatco@benilde.edu.ph>
2025-05-23 18:38:27 +0800

    Added tile generation
    Created a script that generates tiles with gaps between them

---
+++ Transit Tiles/Assets/Scripts/Board.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [Header("Art")]
+    [SerializeField] private Material tileMaterial;
+
+    [Header("Tile Settings")]
+    [SerializeField] private float tileSize = 1f;
+    [SerializeField] private float gapSize = 0.1f;
+
+    private const int TILE_COUNT_X = 8;
+    private const int TILE_COUNT_Y = 8;
+    private GameObject[,] tiles;
+
+    private void Awake()
+    {
+        GenerateAllTiles(tileSize, TILE_COUNT_X, TILE_COUNT_Y);
+    }
+
+    private void GenerateAllTiles(float tileSize, int tileCountX, int tileCountY)
+    {
+        tiles = new GameObject[tileCountX, tileCountY];
+        for (int x = 0; x < tileCountX; x++)
+        {
+            for (int y = 0; y < tileCountY; y++)
+            {
+                tiles[x, y] = GenerateSingleTile(tileSize, x, y);
+            }
+        }
+    }
+
+    private GameObject GenerateSingleTile(float tileSize, int x, int y)
+    {
+        GameObject tileObject = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
+        tileObject.transform.parent = transform;
+
+        Mesh mesh = new Mesh();
+        tileObject.AddComponent<MeshFilter>().mesh = mesh;
+        tileObject.AddComponent<MeshRenderer>().material = tileMaterial;
+
+        float gapOffsetX = x * (tileSize + gapSize);
+        float gapOffsetY = y * (tileSize + gapSize);
+
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(gapOffsetX, 0, gapOffsetY);
+        vertices[1] = new Vector3(gapOffsetX, 0, gapOffsetY + tileSize);
+        vertices[2] = new Vector3(gapOffsetX + tileSize, 0, gapOffsetY);
+        vertices[3] = new Vector3(gapOffsetX + tileSize, 0, gapOffsetY + tileSize);
+
+        int[] tris = new int[] { 0, 1, 2, 1, 3, 2 };
+
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+        mesh.RecalculateNormals();
+
+        tileObject.AddComponent<BoxCollider>();
+
+        return tileObject;
+    }
+}

5c9044ac54d1afbb9dd386cc9277f890254efc69
Grex182 <grecoinigo.yatco@benilde.edu.ph>
2025-06-14 01:07:23 +0800

    Updated Game Manager
    Game Manager now has getters and setters along with the GameState system

--- Transit Tiles/Assets/Scripts/Managers/GameManager.cs
+++ Transit Tiles/Assets/Scripts/Managers/GameManager.cs
@@ -15,10 +15,34 @@ public enum GameState
 
 public class GameManager : Singleton<GameManager>
 {
+    [SerializeField] Board _board;
+
+    public Board Board { get { return _board; } set { _board = value; } }
+
+    [SerializeField] StationManager _stationManager;
+
+    public StationManager StationManager { get { return _stationManager; } set { _stationManager = value; } }
+
+    [SerializeField] PublicRatingManager _publicRatingManager;
+
+    public PublicRatingManager PublicRatingManager { get { return _publicRatingManager; } set { _publicRatingManager = value; } }
+
+    [SerializeField] ScoreManager _scoreManager;
+
+    public ScoreManager ScoreManager { get { return _scoreManager; } set { _scoreManager = value; } }
+
+    [SerializeField] StageSpawner _stageSpawner;
+
+    public StageSpawner StageSpawner { get { return _stageSpawner; } set { _stageSpawner = value; } }
+
+    public static GameManager instance;
+
     public GameState gameState;
 
     private void Awake()
     {
+        instance = this;
+
         DontDestroyOnLoad(gameObject);
     }
 
